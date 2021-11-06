8456f3263a248102b6241b1b5b08d80299d52c5a
Randry1 <revinandr@gmail.com>
2021-11-06 13:33:57 +0700

    Добавил черновики не опубликованные посты кнопку публиковать

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -16,7 +16,8 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+            #Пока удалил дату публикации чтобы создать черновик
+            # post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk = post.pk)
     form = PostForm()
@@ -28,9 +29,21 @@ def post_edit(request, pk ):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+            # Пока удалил дату публикации чтобы создать черновик
+            #post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+def post_draft_list(request):
+    '''Контроллер для показа черновиков постов'''
+    post_draft = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'post_draft': post_draft})
+
+def post_publish(request, pk):
+    '''Обработка нажатия кнопки публикации'''
+
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=post.pk)
