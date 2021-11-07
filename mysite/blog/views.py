ca02a254b859a2926409ef8661b248a48ecdc526
Randry1 <revinandr@gmail.com>
2021-11-08 00:55:19 +0700

    добавил urls для комментария, создал views.py обработку в контроллере, создал шаблон для формы add_comment_to_post.html, создал кнопку для прерхода на форму добавления комментария!!

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Post
+from .models import Post, Comment
 from django.utils import timezone
 from .forms import PostForm, CommentForm
 from django.contrib.auth.decorators import login_required
@@ -72,3 +72,15 @@ def add_comment_to_post(request, pk):
     else:
         form = CommentForm()
         return render(request, 'blog/add_comment_to_post.html', {'form': form})
+
+def comment_approve(request, pk):
+    '''Подтверждение комментария'''
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('post_detail', pk=comment.post.pk)
+
+def comment_remove(request, pk):
+    '''Удаление комментария поста'''
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.delete()
+    return redirect('post_detail', pk=comment.post.pk)
