fce3e240f7b637e8133c63a46cddc547529df31d
Randry1 <revinandr@gmail.com>
2021-11-09 02:04:18 +0700

    добавил urls.py для приложения firstapp, сделал переход с urls.py главного приложения в папку приложеня firstapp, добавил регулярные выражения в path

--- mysite/firstapp/views.py
+++ mysite/firstapp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse('<h1>Главная</h1>')
+
+def about(request):
+    '''Пример реализации обработки запроса пользователя
+    и строки запроса'''
+    return HttpResponse('<h1> О нас</h1>')
+
+def contact(request):
+    ''' Второй пример использования запроса пользлавтеля'''
+    return HttpResponse('<h2> Контакты с нами</h2>')
