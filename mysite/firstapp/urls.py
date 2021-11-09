7278bd7796ae33856569f3d08077b5960b574edc
Randry1 <revinandr@gmail.com>
2021-11-09 22:49:51 +0700

    Извлечение данных из url

--- mysite/firstapp/urls.py
+++ mysite/firstapp/urls.py
@@ -2,8 +2,12 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    path('',views.index, name='index'),
     # path('about/',views.about, name='about'),
     re_path(r'^about', views.about),
     path('contact/',views.contact, name='contact'),
+    path('', views.index, name='index'),
+    re_path(r'^products/(?P<productid>\d+)/', views.products, name='products'),
+    re_path(r'^users/(?P<id>\d+)/(?P<name>\D+)/', views.users, name='users'),
+    #r'^*началозапроса* blank*название функуии во views.py /(?P<blankid>\d+)*описания переменных*/(?P<name>\D+)/(?P<phone>\d+)/'
+    re_path(r'^blanks/(?P<blankid>\d+)/(?P<name>\D+)/(?P<phone>\d+)/', views.blanks, name='blanks'),
 ]
--- mysite/firstapp/views.py
+++ mysite/firstapp/views.py
@@ -13,3 +13,19 @@ def about(request):
 def contact(request):
     ''' Второй пример использования запроса пользлавтеля'''
     return HttpResponse('<h2> Контакты с нами</h2>')
+
+def products(request, productid):
+    '''Функция для изучения получения данных из запроса url адреса
+    http://127.0.0.1:8000/firstapp/produsts/2/'''
+    return HttpResponse("<h1>Продукт № {0}".format(productid))
+
+def users(request, id, name):
+    '''Функйия получения данныйх из URL запроса
+    http://127.0.0.1:8000/firstapp/users/2/Maria/'''
+    return HttpResponse("<h1>Привет {0} ваш id:{1}</h1>".format(name,id))
+
+def blanks(request, blankid, name, phone):
+    '''Моя функция для того чтобы пронять правильно ли получил как вытащить переменные из URL запроса
+    http://127.0.0.1:8000/firstapp/blanks/1/Maria/89089462235/'''
+    page ="<h1>Номер бланка {0}, заполнен на {1}, телефон {2} </h1>".format(blankid, name, phone)
+    return HttpResponse(page)
