8153b2def4ea4753c3f765174c52fc4d888d1427
Randry1 <revinandry@gmail.com>
2021-11-19 23:23:42 +0700

    Основные виды полей в формах

--- mysite/firstapp/forms.py
+++ mysite/firstapp/forms.py
@@ -4,5 +4,21 @@ from django import forms
 
 
 class UserForm(forms.Form):
-    name = forms.CharField(label='Имя')
+    name = forms.CharField(label='Имя', required=True)
     age = forms.IntegerField(label='Возраст')
+    sex = forms.BooleanField(label='Пол')
+    email_send = forms.NullBooleanField(label='Подписка', required=False)
+    email = forms.EmailField(label='Емайл')
+    combo = forms.ComboField(fields=[age, sex])
+    multi = forms.MultiValueField(fields=[email, name])
+    file = forms.FileField(label='Документ')
+    image = forms.ImageField(label='Аватар')
+    date = forms.DateTimeField(label='Дата рождения')
+    decimal = forms.DecimalField(label='Счет')
+    hair_lengh = forms.FloatField(label='Длинна прически')
+    foot_size = forms.ChoiceField(label='Размер ноги', choices=(
+        (1, "L"),
+        (2, "XL"),
+        (3, "XXL"),
+        (4, "XXXL"),
+    ))
