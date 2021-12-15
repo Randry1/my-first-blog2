9ceef75ab81c9a70edf3c1b7a92d12525f774bbd
Randry1 <revinandry@gmail.com>
2021-12-16 05:06:57 +0700

    add bio in model Person

--- mysite/firstapp/models.py
+++ mysite/firstapp/models.py
@@ -4,3 +4,4 @@ class Person(models.Model):
     """Первый раза использую БД глава из книжки"""
     name = models.CharField(max_length=12)
     age = models.IntegerField()
+    bio = models.CharField(max_length=300, default='')
