b0718209942a3cbae1a27a5f0696c7916d6027aa
Randry1 <revinandry@gmail.com>
2022-01-03 07:33:36 +0700

    Начало создание моей модели всех электриков

--- mysite/firstapp/urls.py
+++ mysite/firstapp/urls.py
@@ -67,4 +67,5 @@ urlpatterns = [
     path('create', views.create, name='create'), # Вывод всех данных из модели
     re_path(r'^edit/(?P<id_person>\d+)/', views.edit, name='edit'), # изменение данных из модели
     path('delete/<int:id>/', views.delete, name='delete'), # удаление данных из модели
+    path('electric_index', views.electric_index, name='electric_index') #Файл создания новых электриков, и вывода всех электриков
 ]
