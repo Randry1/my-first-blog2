93e4c1af8284dac0d15de80d2c798b5f1a9f3601
Randry1 <revinandry@gmail.com>
2022-01-16 12:01:22 +0700

    add models связь один ко многим

--- mysite/firstapp/models.py
+++ mysite/firstapp/models.py
@@ -16,3 +16,12 @@ class Electric(models.Model):
     bio = models.CharField(blank=True, help_text='О себе', max_length=230)
     active = models.BooleanField(default=True, help_text='Работает')
 
+class Forest(models.Model):
+    """Класс лес для  показа модели отношение один ко многим"""
+    name = models.CharField(max_length=15, help_text="Имя леса")
+
+class Tree(models.Model):
+    """Класс дерево, показать отношнение один ко многим"""
+    forest = models.ForeignKey(Forest, on_delete=models.CASCADE)
+    name = models.CharField(max_length=15, help_text='Название дерева')
+    height = models.IntegerField()
